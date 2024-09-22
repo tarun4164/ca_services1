34d032170351bdfe16d3d576a145a8b15a0cddb5
Tarun <tarunbommu@gmail.com>
2024-09-22 11:43:54 +0530

    styles updated

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -9,8 +9,8 @@ const Hero = ()=>{
                 <div className="hero__row">
                     <div className="hero__content-info">
                         <div className="hero__header">CA serviceses heading</div>
-                        <div className="hero__description">CA serviceses description</div>
-                        <div className="hero__button">CA Buttonn</div>
+                        <div className="hero__description">Professional CA services to manage your accounting and financial needs efficiently.</div>
+                        <div className="hero__button">Get Started</div>
                     </div>
                     <div className="hero__image-container">
                         < img className="hero__img" src={caservices} alt="hero-photo"/>
--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,64 +1,68 @@
+import React from "react";
 import "./Services.scss";
+import auditingImg from "../../assets/auditing.png"
+
 export const Services = () => {
   return (
-    <section>
+    <section className="services-section">
       <ul className="services">
         <li className="service__row">
           <div className="service__container">
             <div className="service__left">
               <h3 className="service__header">Auditing and Assurance</h3>
               <div className="service__link">
-                <div>Icon</div>
-                <div>Learn more</div>
+                <div className="service__icon"></div>
+                <a href="#" className="service__learn-more">Learn more</a>
               </div>
             </div>
             <div className="service__right">
-              <img src="#" alt="some-image" />
+              <img src={auditingImg} alt="Auditing and Assurance" className="service__img" />
             </div>
           </div>
         </li>
         <li className="service__row">
           <div className="service__container">
             <div className="service__left">
-              <h3 className="service__header">Taxation Services</h3>
+              <h3 className="service__header">GST</h3>
               <div className="service__link">
-                <div>Icon</div>
-                <div>Learn more</div>
+                <div className="service__icon"></div>
+                <a href="#" className="service__learn-more">Learn more</a>
               </div>
             </div>
             <div className="service__right">
-              <img src="#" alt="some-image" />
+              <img src={auditingImg} alt="Auditing and Assurance" className="service__img" />
             </div>
           </div>
         </li>
         <li className="service__row">
           <div className="service__container">
             <div className="service__left">
-              <h3 className="service__header">Financial Advisory</h3>
+              <h3 className="service__header">ITR</h3>
               <div className="service__link">
-                <div>Icon</div>
-                <div>Learn more</div>
+                <div className="service__icon"></div>
+                <a href="#" className="service__learn-more">Learn more</a>
               </div>
             </div>
             <div className="service__right">
-              <img src="#" alt="some-image" />
+              <img src={auditingImg} alt="Auditing and Assurance" className="service__img" />
             </div>
           </div>
         </li>
         <li className="service__row">
           <div className="service__container">
             <div className="service__left">
-              <h3 className="service__header">Accounting Services</h3>
+              <h3 className="service__header">Company Registration</h3>
               <div className="service__link">
-                <div>Icon</div>
-                <div>Learn more</div>
+                <div className="service__icon"></div>
+                <a href="#" className="service__learn-more">Learn more</a>
               </div>
             </div>
             <div className="service__right">
-              <img src="#" alt="some-image" />
+              <img src={auditingImg} alt="Auditing and Assurance" className="service__img" />
             </div>
           </div>
         </li>
+        {/* Additional services follow the same structure */}
       </ul>
     </section>
   );
